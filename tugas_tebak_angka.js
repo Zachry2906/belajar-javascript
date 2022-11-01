e7da393a8fb4b072b1b5a805fde5d0744fc8b028
Zachry2906 <aryazakaria67@gmail.com>
2022-11-02 00:40:16 +0700

    membuat program tebak nomor js pk sandika

---
+++ tugas_tebak_angka.js
@@ -0,0 +1,41 @@
+var nilai = false;
+
+alert(" Tebak angka 1 - 10!\n Kamu punya 3x kesempatan ");
+angkaTebak = prompt(" Masukkan angka ");
+
+var algo = Math.floor(Math.random() * 11);
+
+if (angkaTebak < algo) {
+  hasil = "Tebakan terlalu rendah";
+} else if (angkaTebak > algo) {
+  hasil = "Tebakan terlalu tinggi";
+} else {
+  hasil = "Tebakan Benar";
+}
+alert(hasil);
+if (hasil === "Tebakan terlalu rendah" || hasil == "Tebakan terlalu tinggi") {
+  angkaTebak = prompt(" Coba lagi : \n Kesempatan sisa 2 ");
+  if (angkaTebak < algo) {
+    hasil = "Tebakan terlalu rendah";
+  } else if (angkaTebak > algo) {
+    hasil = "Tebakan terlalu tinggi";
+  } else {
+    hasil = "Tebakan Benar";
+  }
+
+  alert(hasil);
+  if (hasil === "Tebakan terlalu rendah" || hasil == "Tebakan terlalu tinggi") {
+    angkaTebak = prompt(" Coba lagi : \n Kesempatan sisa 1 ");
+    if (angkaTebak < algo) {
+      hasil = "Tebakan terlalu rendah";
+    } else if (angkaTebak > algo) {
+      hasil = "Tebakan terlalu tinggi";
+    } else {
+      hasil = "Tebakan Benar";
+    }
+    alert(hasil);
+    if (hasil === "Tebakan terlalu rendah" || hasil == "Tebakan terlalu tinggi") {
+      alert(" sisa coba hasbis");
+    }
+  }
+}
