ddeddd74e7a19c77ccf40ecd3504ffd53e41c4b7
Zachry2906 <aryazakaria67@gmail.com>
2022-11-09 14:45:08 +0700

    mengerjakan studi fungsi dan array

---
+++ angkotarrayfunc.js
@@ -0,0 +1,36 @@
+var penumpang = [];
+var tambahPenumpang = function (namaPenumpang, penumpang) {
+  if (penumpang.length == 0) {
+    penumpang.push(namaPenumpang);
+    return penumpang;
+  } else {
+    for (let i = 0; i < penumpang.length; i++) {
+      if (penumpang[i] == undefined) {
+        penumpang[i] = namaPenumpang;
+        return penumpang;
+      } else if (penumpang[i] == namaPenumpang) {
+        console.log(namaPenumpang + "sudah masuk dalam angkot");
+        return penumpang;
+      } else if (i == penumpang.length - 1) {
+        penumpang.push(namaPenumpang);
+        return penumpang;
+      }
+    }
+  }
+};
+
+var hapusPenumpang = function (namaPenumpang, penumpang) {
+  for (let i = 0; i < penumpang.length; i++) {
+    if (penumpang.length == 0) {
+      console.log("Angkot kosong, belum terisi");
+      return penumpang;
+    } else {
+      if (penumpang[i] == namaPenumpang) {
+        penumpang[i] = undefined;
+      } else if (penumpang[i] !== namaPenumpang) {
+        console.log(namaPenumpang + "tidak ada nama tersebut");
+        return penumpang;
+      }
+    }
+  }
+};
