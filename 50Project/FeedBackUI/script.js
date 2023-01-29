6935feb71e4186554320d71d3e00c8d907926231
Zachry2906 <aryazakaria67@gmail.com>
2023-01-29 23:52:42 +0700

    FeedBackUI

---
+++ 50Project/FeedBackUI/script.js
@@ -0,0 +1,9 @@
+const ratings = document.querySelectorAll('.rating')
+const sendBtn = document.querySelector('#send')
+const panel = document.querySelector('#panel')
+
+panel.addEventListener('click', function(e){
+    if(e.target.parentNode.classList.contains('rating')){
+        
+    }
+})
