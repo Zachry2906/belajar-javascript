cdff6e9d8e38959ba1c1fb6e7732ce863d9033e0
Zachry2906 <aryazakaria67@gmail.com>
2023-01-30 21:04:46 +0700

    FeednabkUI dan Countdown

--- 50Project/FeedBackUI/script.js
+++ 50Project/FeedBackUI/script.js
@@ -1,9 +1,29 @@
 const ratings = document.querySelectorAll('.rating')
+const ratingsContainer = document.querySelector('.rating-container')
 const sendBtn = document.querySelector('#send')
 const panel = document.querySelector('#panel')
+let selectedRating = 'Satisfied'
 
-panel.addEventListener('click', function(e){
+ratingsContainer.addEventListener('click', function(e){
     if(e.target.parentNode.classList.contains('rating')){
-        
+        removeActive()
+        e.target.parentNode.classList.add('active')
+        selectedRating = e.target.innerHTML
     }
 })
+
+sendBtn.addEventListener('click', (e) => {
+    panel.innerHTML = ` 
+    <i class="fas fa-heart"></i>
+    <strong> Thank You </strong>
+    <br>
+    <strong> Feedback: ${selectedRating} </strong>
+    <p>We'll use your feedback to improve our <br> customer support</p>
+    `
+})
+
+function removeActive() {
+    for(let i = 0; i < ratings.length; i++){
+        ratings[i].classList.remove('active')
+    }
+}
